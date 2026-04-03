672f993f2c05f37d87848603d073c8eac866e145
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-04-03 16:58:26 -0300

    Pilares da orientação a objeto - Abstração

---
+++ src/main/java/ContaPoupanca.java
@@ -0,0 +1,11 @@
+public class ContaPoupanca {
+    private int agencia;
+    private int numeroConta;
+    private double saldo;
+
+    public void sacar() {}
+
+    public void depositar() {}
+
+    public void transferir() {}
+}

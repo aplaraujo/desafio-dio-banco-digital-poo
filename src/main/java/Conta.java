77080dc0d850d60056622e16f291832ed09d80b1
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-04-03 17:13:40 -0300

    Pilares da orientação a objetos - Herança

---
+++ src/main/java/Conta.java
@@ -0,0 +1,32 @@
+public abstract class Conta implements IConta {
+    private int agencia;
+    private int numeroConta;
+    private double saldo;
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+
+    }
+}
--- src/main/java/ContaPoupanca.java
+++ src/main/java/ContaPoupanca.java
@@ -1,23 +1,2 @@
-public class ContaPoupanca {
-    private int agencia;
-    private int numeroConta;
-    private double saldo;
-
-    public int getAgencia() {
-        return agencia;
-    }
-
-    public int getNumeroConta() {
-        return numeroConta;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void sacar() {}
-
-    public void depositar() {}
-
-    public void transferir() {}
+public class ContaPoupanca extends Conta{
 }
